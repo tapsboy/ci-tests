66911d04ae2c5a46f68d56ef1cc07eca47d7784a
Tapan Shah <tapsboy@gmail.com>
2017-05-18 19:05:02 -0400

    Update index

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('CI TESTS')
+require('http');
+
+var server = http.createServer(function (request, response) {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.end("CI Tests\n");
+});
+
+server.listen(80);
+
+console.log("Server running at http://127.0.0.1:8000/");
