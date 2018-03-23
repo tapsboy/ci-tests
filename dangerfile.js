b8f82fa113afcc9332eb2a798450baee72242e2c
Tapan Shah <tapsboy@gmail.com>
2018-03-23 18:48:38 -0400

    Console output

--- dangerfile.js
+++ dangerfile.js
@@ -1,5 +1,6 @@
 const {message, danger} = require("danger")
 
 const modifiedMD = danger.git.modified_files.join("- ")
+console.log("Changed Files in this PR: \n - " + modifiedMD);
 message("Changed Files in this PR: \n - " + modifiedMD)
 
