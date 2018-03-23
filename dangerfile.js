c11e10b257f0e5f4a8767a2fe9c2624fe07f12e4
Tapan Shah <tapsboy@gmail.com>
2018-03-23 18:15:18 -0400

    imports fix

--- dangerfile.js
+++ dangerfile.js
@@ -1,4 +1,4 @@
-import {message, danger} from "danger"
+const {message, danger} = require("danger")
 
 const modifiedMD = danger.git.modified_files.join("- ")
 message("Changed Files in this PR: \n - " + modifiedMD)
