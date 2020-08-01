59212892d450cdbaed2279875d7155968244d0c1
Tapan Shah <tapsboy@gmail.com>
2020-08-01 14:07:58 -0400

    fix: cleanup

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-var http = require('http');
+const http = require('http');
 
-var server = http.createServer(function (request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("CI Tests\n");
+const server = http.createServer(function (request, response) {
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+  response.end("CI Tests: Hello from server.js\n");
 });
 
 server.listen(80);
 
-console.log("Server running at http://127.0.0.1:8000/");
+console.log('Server running at http://127.0.0.1:80/');
