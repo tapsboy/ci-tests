4cfb00cb88a3dc5ffcbd29a1daaa688141a56ae8
Tapan Shah <tapsboy@gmail.com>
2020-08-01 23:46:07 -0400

    add prettier and add new feature PR

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const http = require('http');
 
 const server = http.createServer(function (request, response) {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end("CI Tests: Hello from server.js\n");
+  response.writeHead(200, { 'Content-Type': 'text/plain' });
+  response.end('CI Tests: Hello from server.js. Attempt new feature PR\n');
 });
 
 server.listen(8080);
