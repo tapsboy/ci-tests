51ac19d28d74b9827d04835cf7ef198202f89ca3
Tapan Shah <tapsboy@gmail.com>
2020-08-02 00:02:10 -0400

    fix: add logging for each request

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const http = require('http');
 
 const server = http.createServer(function (request, response) {
+  console.log('Request received');
   response.writeHead(200, { 'Content-Type': 'text/plain' });
   response.end('CI Tests: Hello from server.js. Attempt new feature PR\n');
 });
