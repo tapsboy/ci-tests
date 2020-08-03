6a5bea75fd7be632deab1f76cc8b288e285705f9
Tapan Shah <tapsboy@gmail.com>
2020-08-03 02:18:38 -0400

    chore: update msg

--- server.js
+++ server.js
@@ -3,7 +3,10 @@ const http = require('http');
 const server = http.createServer(function (request, response) {
   console.log('Request received');
   response.writeHead(200, { 'Content-Type': 'text/plain' });
-  response.end('CI Tests: Hello from server.js. Attempt new feature PR\n');
+  response.end(`
+  CI Tests: Hello from server.js.
+  Attempt new feature PR with FeaturePeek.
+  Attempt Cypress integration`);
 });
 
 server.listen(8080);
